02df7937ca496e169db8cf0f768d0e455fcf53d5
seunghwan Choi <dexrf1@gmail.com>
2022-03-17 22:17:48 +0900

    apply builder pattern to Entity classes

--- src/main/java/crud/website/domain/Post.java
+++ src/main/java/crud/website/domain/Post.java
@@ -1,10 +1,14 @@
 package crud.website.domain;
 
 import com.sun.istack.NotNull;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
+@RequiredArgsConstructor
+@Builder
 public class Post extends BaseTimeEntity{
 
     @Id @GeneratedValue
--- src/main/java/crud/website/domain/User.java
+++ src/main/java/crud/website/domain/User.java
@@ -1,6 +1,8 @@
 package crud.website.domain;
 
 import com.sun.istack.NotNull;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,6 +10,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@RequiredArgsConstructor
+@Builder
 public class User {
 
     @Id @GeneratedValue
