b8952853a3abc1d35191f532b0790ba55c4a5992
seunghwan Choi <dexrf1@gmail.com>
2022-03-21 20:42:09 +0900

    feat: add findByEmail method and existsByEmail, existsByNickname method in MemberRepository

--- src/main/java/crud/website/Repository/MemberRepository.java
+++ src/main/java/crud/website/Repository/MemberRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Repository
@@ -17,6 +18,33 @@ public class MemberRepository {
 
     public Member findById(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public Member findByEmail(MemberDto memberDto) {
+        List resultList = em.createQuery("select m from Member m where m.email= :email")
+                .setParameter("email", memberDto.getEmail())
+                .getResultList();
+
+//        if (resultList.size() != 1)
+//            throw new IllegalStateException("아이디와 일치하는 회원이 없습니다.");
+
+        return (Member) resultList.get(0);
+    }
+
+    public boolean existsByEmail(MemberDto memberDto) {
+        List resultList = em.createQuery("select m from Member m where m.email= :email")
+                .setParameter("email", memberDto.getEmail())
+                .getResultList();
+
+        return resultList.size() != 0;
+    }
+
+    public boolean existsByNickname(MemberDto memberDto) {
+        List resultList = em.createQuery("select m from Member m where m.nickname= :nickname")
+                .setParameter("nickname", memberDto.getNickname())
+                .getResultList();
+
+        return resultList.size() != 0;
     }
 
     public Member save(MemberDto userDto) {
